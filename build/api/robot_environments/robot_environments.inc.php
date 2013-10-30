6cea54eaa8c355bcab15495fa48952cc4064a36a
Dan Lazewatsky <dan@lazewatsky.com>
2013-10-30 12:14:32 -0500

    fixed some stylesheets from robotics.oregonstate.edu that disappeared

--- build/api/robot_environments/robot_environments.inc.php
+++ build/api/robot_environments/robot_environments.inc.php
@@ -364,7 +364,7 @@ class robot_environments
         global $title;
         $pagename = 'Invalid Connection';
         $prot = (isset($_SERVER['HTTPS'])) ? 'https://' : 'http://';
-        $path = $prot.$_SERVER['HTTP_HOST'].'/rms/';
+        $path = $prot.$_SERVER['HTTP_HOST'].'/';
 
         echo '
                 <!DOCTYPE html>
